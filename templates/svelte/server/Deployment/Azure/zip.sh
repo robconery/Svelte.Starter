0be4937172e9ee22e5ddf0f64525b6c16b349727
Aaron Powell <me@aaron-powell.com>
2023-05-04 11:05:39 +1000

    adding az cli scripts back in

---
+++ templates/svelte/server/Deployment/Azure/zip.sh
@@ -0,0 +1,10 @@
+rm ./Deployment/Azure/deploy.zip
+rm -R bin/Release
+dotnet publish --configuration Release
+cd bin/Release/net7.0/publish/
+zip -r ../../../../Deployment/Azure/deploy.zip . -q
+cd -
+az webapp deployment source config-zip --resource-group svelte-starter --name svelte-starter-2298 --src ./Deployment/Azure/deploy.zip
+open https://svelte-starter-2298.azurewebsites.net
+echo 'Site is published 🎉 watching logs. It takes 60 seconds or so to refresh...'
+az webapp log tail -n svelte-starter-2298 -g svelte-starter

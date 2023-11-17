6c52d71aa81227f001118aa1eac15f2310530413
Michal Krchnavy <michal.krchnavy@outlook.com>
2023-11-17 21:17:49 +0100

    Removes the hard depencency on the API service

--- templates/svelte/app/vite.config.js
+++ templates/svelte/app/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 4444
+		port: 4444,
+        proxy: {
+            '/api': 'http://localhost:8000',
+        },		
 	}
 });
